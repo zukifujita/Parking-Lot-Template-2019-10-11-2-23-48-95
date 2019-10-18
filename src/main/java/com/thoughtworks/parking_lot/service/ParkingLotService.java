e8a84e4b4059624e4702111c46d83369240ade31
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-18 22:47:30 +0800

    Added deleteParkingLot to Service

--- src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
+++ src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
@@ -2,15 +2,28 @@ package com.thoughtworks.parking_lot.service;
 
 import com.thoughtworks.parking_lot.core.ParkingLot;
 import com.thoughtworks.parking_lot.repository.ParkingLotRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParkingLotService {
+
+    private static final String RECORD_NOT_FOUND = "Record not found!";
     @Autowired
     ParkingLotRepository parkingLotRepository;
 
     public ParkingLot addParkingLot(ParkingLot parkingLot) {
         return parkingLotRepository.save(parkingLot);
+    }
+
+    public ParkingLot deleteParkingLot(Integer id) throws NotFoundException {
+        ParkingLot parkingLotFromDb = parkingLotRepository.findById(id).orElse(null);
+
+        if (parkingLotFromDb != null) {
+            parkingLotRepository.delete(parkingLotFromDb);
+        }
+
+        throw new NotFoundException(RECORD_NOT_FOUND);
     }
 }
