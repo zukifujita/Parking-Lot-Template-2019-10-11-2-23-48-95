c6f474a1e3f8a69c9a13752fc26b66e479dee734
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-18 23:29:31 +0800

    Modified Repository parameters to String name

--- src/main/java/com/thoughtworks/parking_lot/repository/ParkingLotRepository.java
+++ src/main/java/com/thoughtworks/parking_lot/repository/ParkingLotRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ParkingLotRepository extends JpaRepository<ParkingLot, Integer> {
+public interface ParkingLotRepository extends JpaRepository<ParkingLot, String> {
 }
