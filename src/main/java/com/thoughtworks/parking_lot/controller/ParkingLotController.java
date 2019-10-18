19f1156691210f921f3055c2d439a23e15a69f47
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-18 22:38:55 +0800

    Added ParkingLotController

--- src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
+++ src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
@@ -1,4 +1,21 @@
 package com.thoughtworks.parking_lot.controller;
 
+import com.thoughtworks.parking_lot.core.ParkingLot;
+import com.thoughtworks.parking_lot.service.ParkingLotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/parking-lot")
 public class ParkingLotController {
+
+    @Autowired
+    ParkingLotService parkingLotService;
+
+    @PostMapping(produces = {"application/json"})
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public ParkingLot addParkingLot(@RequestBody ParkingLot parkingLot) {
+        return parkingLotService.addParkingLot(parkingLot);
+    }
 }
