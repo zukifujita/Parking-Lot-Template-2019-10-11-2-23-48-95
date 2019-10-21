4790ef957cee60677b39a7c349acff0edb6c7a6a
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-22 00:58:31 +0800

    Covered most of the scenarios in ParkingLotControllerTest

--- src/test/java/com/thoughtworks/parking_lot/controller/ParkingLotControllerTest.java
+++ src/test/java/com/thoughtworks/parking_lot/controller/ParkingLotControllerTest.java
@@ -13,15 +13,16 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -69,7 +70,7 @@ public class ParkingLotControllerTest {
 
         when(parkingLotService.deleteParkingLot(anyString())).thenReturn(parkingLot);
 
-        ResultActions resultOfExecution = mvc.perform(delete("/parking-lot/{id}", 1L));
+        ResultActions resultOfExecution = mvc.perform(delete("/parking-lot/{name}", "ParkingLotTest"));
 
         resultOfExecution.andExpect(status().isOk());
     }
@@ -78,6 +79,43 @@ public class ParkingLotControllerTest {
     void should_show_parking_lot_when_no_name_is_specified() throws Exception {
         ParkingLot parkingLot = createParkingLot("ParkingLotTest");
 
-        when(parkingLotService.showAllParkingLotsByPage(anyInt(), anyInt()));
+        LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("page", "1");
+        requestParams.add("pageSize", "5");
+
+        parkingLotList.add(parkingLot);
+
+        when(parkingLotService.showAllParkingLotsByPage(anyInt(), anyInt())).thenReturn(parkingLotList);
+
+        ResultActions resultOfExecution = mvc.perform(get("/parking-lot").params(requestParams));
+
+        resultOfExecution.andExpect(status().isOk()).andExpect(jsonPath("$[0].name", is("ParkingLotTest")));
+    }
+
+    @Test
+    void should_show_parking_lot_when_entered_specific_name() throws Exception {
+        ParkingLot parkingLot = createParkingLot("ParkingLotTest");
+
+        when(parkingLotService.showParkingLotByName(anyString())).thenReturn(parkingLot);
+
+        ResultActions resultOfExecution = mvc.perform(get("/parking-lot/{name}", "ParkingLotTest"));
+
+        resultOfExecution.andExpect(status().isOk());
+    }
+
+    @Test
+    void should_update_parking_lot_when_new_detail_is_entered() throws Exception {
+        ParkingLot parkingLot = createParkingLot("ParkingLotTest");
+        ParkingLot newParkingLot = createParkingLot("ParkingLotTest2");
+
+        when(parkingLotService.editParkingLot(anyString(), any())).thenReturn(newParkingLot);
+
+        ResultActions resultOfExecution = mvc.perform(patch("/parking-lot/{name}", "ParkingLotTest")
+                .content(objectMapper.writeValueAsString(parkingLot))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        resultOfExecution.andExpect(status().isOk())
+            .andExpect(jsonPath("$.name" , is(newParkingLot.getName())));
     }
 }
